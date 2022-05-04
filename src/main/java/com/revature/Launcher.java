fdfe9c1d14dbae579d5dc3bb06e02bab806981ca
melbmar <archi.sanchez@gmail.com>
2022-05-04 14:27:11 -0400

    Frontandjavacode

--- src/main/java/com/revature/Launcher.java
+++ src/main/java/com/revature/Launcher.java
@@ -1,5 +1,10 @@
 package com.revature;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import com.revature.utils.ConnectionUtil;
+
 public class Launcher {
 
     public static void main(String[] args) {
@@ -20,10 +25,19 @@ public class Launcher {
     	  
     	  Here's an aardvark this time... not as friendly or cute as a dog, but take him anyway.
     	  // Nice! ^_-
+    	   
     	   * 
     	   *
     	   
     	 */
+    	        //In this try/catch, we're just testing whether our Connection (from the ConnectionUtil Class) is successful
+    			//The getConnection() method will return a Connection Object if you connect successfully
+    			try(Connection conn = ConnectionUtil.getConnection()){
+    				System.out.println("CONNECTION SUCCESSFUL :)");
+    			} catch (SQLException e) { //if creating this connection fails... catch the exception and print the stack trace
+    				System.out.println("connection failed... :(");
+    				e.printStackTrace();
+    			}
     	
     }
 }
